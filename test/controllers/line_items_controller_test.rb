e7828c7d9c9c41fede2c28347e37af87f3523594
Noelia Lencina <noelia@wyeworks.com>
2017-07-24 10:57:20 -0300

    Chapter 10

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -21,8 +21,9 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     end
 
     follow_redirect!
-    assert_select 'h2', 'Your Pragmatic Cart'
-    assert_select '#main li', 'Agile Web Development with Rails 5'
+
+    assert_select 'h2', 'Your Cart'
+    assert_select 'td', "Agile Web Development with Rails 5"
   end
 
   test "should show line_item" do
@@ -36,7 +37,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update line_item" do
-    patch line_item_url(@line_item), params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
+    patch line_item_url(@line_item), params: { line_item: { product_id: @line_item.product_id } }
     assert_redirected_to line_item_url(@line_item)
   end
 
