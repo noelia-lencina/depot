bc50f89889e31e605055c69cdbf7c0961ff47bce
Noelia Lencina <noelia@wyeworks.com>
2017-08-01 11:24:42 -0300

    Iteration H1 finished

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require_relative 'application'
 
+Depot::Application.configure do 
+  config.action_mailer.delivery_method = :test
+
+end
 # Initialize the Rails application.
 Rails.application.initialize!
---
+++ test/mailers/order_mailer_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class OrderMailerTest < ActionMailer::TestCase 
+  test "received" do
+    mail = OrderMailer.received(orders(:one))
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    assert_match /1 x Agile Web Development with Rails 5/, mail.body.encoded
+  end
+
+  test "shipped" do
+    mail = OrderMailer.shipped(orders(:one))
+    assert_equal "Pragmatic Store Order Shipped", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    assert_match /<td>1&times;<\/td>\s*<td>Agile Web Development with Rails 5<\/td>/, mail.body.encoded
+  end 
+end
